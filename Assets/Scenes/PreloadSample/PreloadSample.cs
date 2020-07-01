6a20d77db7dd58975680fd5de343ff496200045f
enu <otoriden@gmail.com>
2020-07-01 10:42:10 +0900

    PreloadSample

---
+++ Assets/Scenes/PreloadSample/PreloadSample.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TSKT
+{
+    public class PreloadSample : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Debug.Log("awake");
+        }
+        void Start()
+        {
+            Debug.Log("start");
+            Scenes.SceneUtil.Preload("PreloadSample");
+            Scenes.SceneUtil.Preload("PreloadSample3");
+            Scenes.SceneUtil.Preload("PreloadSample2");
+
+            Scenes.SceneUtil.Load("PreloadSample2");
+        }
+    }
+}
