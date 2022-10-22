c66d4481e3fcd0cd84ad3d26bd41dcfeef15ec12
enue <otoriden@gmail.com>
2022-10-22 20:51:35 +0900

    unity2023.1.0aでエラーになるのを修正

--- Assets/Package/Runtime/Process.cs
+++ Assets/Package/Runtime/Process.cs
@@ -1,9 +1,9 @@
-﻿using UnityEngine;
+﻿#nullable enable
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using Cysharp.Threading.Tasks;
-#nullable enable
 
 namespace TSKT.Scenes
 {
@@ -73,7 +73,7 @@ namespace TSKT.Scenes
         readonly public async UniTask<Scene> Execute()
         {
             operation.allowSceneActivation = true;
-            await operation;
+            await operation.ToUniTask();
             var scene = SceneManager.GetSceneByName(sceneName);
             if (!scene.IsValid())
             {
@@ -112,7 +112,7 @@ namespace TSKT.Scenes
 
             if (waitUnload)
             {
-                await unloadTask;
+                await unloadTask.ToUniTask();
                 _ = Resources.UnloadUnusedAssets();
             }
             else
@@ -194,11 +194,11 @@ namespace TSKT.Scenes
         static public async UniTask Execute(Scene scene)
         {
             var sceneIndex = scene.buildIndex;
-            await SceneManager.UnloadSceneAsync(scene);
+            await SceneManager.UnloadSceneAsync(scene).ToUniTask();
 
             var loadOperation = SceneManager.LoadSceneAsync(sceneIndex, LoadSceneMode.Additive);
             LoadingProgress.Instance.Add(loadOperation, 1f);
-            await loadOperation;
+            await loadOperation.ToUniTask();
             SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(sceneIndex));
             _ = Resources.UnloadUnusedAssets();
         }
